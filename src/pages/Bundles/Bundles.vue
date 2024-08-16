62c8f04a49e95b0e37046178fba08385a0775693
JorgeAguilar17 <jorge.aguilar@xebelsoft.com>
2024-08-16 17:21:54 -0600

    Arreglo de  baner de categoria en ofertas y paquetes

--- src/pages/Bundles/Bundles.vue
+++ src/pages/Bundles/Bundles.vue
@@ -4,16 +4,50 @@
     <div class="p-grid crud-demo" v-if="!skeleton">
         <div class="col-12">
             <div class="grid formgrid p-fluid">
-                <div class="field hidden md:block col-12 xl:col-2">
-                    <!-- Definitivamente hacerlo componente -->
+                <div class="field col-12 hidden md:block">
+                    <div class="field w-full">
+                        <div class=" full-width-sm-index">
+                        <div class="p-3 flex justify-content-center align-items-center flex-wrap">
+                            <div class="justify-content-center align-items-center flex">
+                                <span class="w-full text-8xl">
+                                    {{ pagetitle.toUpperCase() }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <!-- <div class="field hidden md:block col-12 xl:col-2">
+                    Definitivamente hacerlo componente
                     <span class="text-900 font-medium text-4xl mb-4">{{ pagetitle }}</span>
                     <br>
                     <span class="text-900 font-medium text-xs mb-4">{{  products.length }} Productos Relacionados</span>
                     <br>
                     <br>
                     <BasicFilterSelection @v-model="getfilterprices" :key="componentKey" :allcategories="priceorder" filtername="Filtro por Precios" pickone/>
+                </div> -->
+                <div class="field hidden sm:block col-2">
+                    <!-- Definitivamente hacerlo componente -->
+                    <br>
+                    <span class="text-900 font-medium text-xs mb-4">{{ products.length }} Productos Relacionados</span>
+                    <br>
+                    <br>
+                    <BasicFilterSelection @v-model="getfilterprices" :key="componentKey" :allcategories="priceorder" filtername="Filtro por Precios" pickone
+                    :selected="filterpriceselection"/>
                 </div>
-                <div class="field col-12 sm:col-12 md:col-12 xl:col-10">
+                <div class="field block sm:hidden col-12">
+                    <Sidebar v-model:visible="visible">
+                        <span class="text-900 font-medium text-4xl mb-4">{{ pagetitle }}</span>
+                        <br>
+                        <span class="text-900 font-medium text-xs mb-4">{{ products.length }} Productos Relacionados</span>
+                        <br>
+                        <br>
+                        <BasicFilterSelection @v-model="getfilterprices" :key="componentKey" :allcategories="priceorder" filtername="Filtro por Precios" pickone
+                        :selected="filterpriceselection"/>
+                    </Sidebar>
+                    <Button icon="pi pi-filter" class="filtter-menu-button border-transparent border-1 shadow-3" @click="visible = true" />
+                </div>
+                <div class="field col-12 sm:col-10">
                     <div class="card">
                         <br>
                         <!-- Agregar paginator obligatorio -->
@@ -47,6 +81,7 @@ const route = useRoute();
 const router = useRouter();
 const entity = new OrderData();
 const cartStore = useCartStore();
+const visible = ref<boolean>(false)
 
 onMounted(async () => {
     await refresh();
@@ -70,11 +105,12 @@ const refresh = async () => {
 
 watch(filterpriceselection, (newvalue)=>{
     if(newvalue[0] == 1){
-        products.value = products.value.sort((a,b) => a.total - b.total)
+        products.value = products.value.sort((a,b) => a.price_tax - b.price_tax)
     }else if(newvalue[0] == 2){
-        products.value = products.value.sort((a,b) => b.total - a.total)
+        products.value = products.value.sort((a,b) => b.price_tax - a.price_tax)
     }
     productsKey.value = productsKey.value+1;
+    componentKey.value = componentKey.value+1;
 })
 
 watch(router.currentRoute, async ()=>{
@@ -86,3 +122,17 @@ const getfilterprices = (model) =>{
 }
 
 </script>
+
+<style>
+    .full-width-sm-index {
+  background-image: url('/public/demo/images/pages/fondo dental-01.jpg');
+  background-size: cover;
+  background-position: center;
+  padding: 50px 20px; /* Reducir padding en pantallas pequeñas */
+  color: rgb(255, 255, 255);
+  display: flex;
+  flex-direction: column; /* Cambiar a columna para centrar verticalmente en pantallas pequeñas */
+  align-items: center; /* Center horizontally */
+  text-align: center; /* Center text within each column */
+    }
+</style>
--- src/pages/Offers/Offers.vue
+++ src/pages/Offers/Offers.vue
@@ -4,16 +4,41 @@
     <div class="p-grid crud-demo" v-if="!skeleton">
         <div class="col-12">
             <div class="grid formgrid p-fluid">
-                <div class="field hidden md:block col-12 xl:col-2">
-                    <!-- Definitivamente hacerlo componente -->
-                    <span class="text-900 font-medium text-4xl mb-4">{{ pagetitle }}</span>
-                    <br>
-                    <span class="text-900 font-medium text-xs mb-4">{{  products.length }} Productos Relacionados</span>
-                    <br>
-                    <br>
-                    <BasicFilterSelection @v-model="getfilterprices" :key="componentKey" :allcategories="priceorder" filtername="Filtro por Precios" pickone/>
+                <div class="field col-12 hidden md:block">
+                    <div class="field w-full">
+                        <div class=" full-width-sm-index">
+                        <div class="p-3 flex justify-content-center align-items-center flex-wrap">
+                            <div class="justify-content-center align-items-center flex">
+                                <span class="w-full text-8xl">
+                                    {{ pagetitle.toUpperCase() }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                <div class="field col-12 sm:col-12 md:col-12 xl:col-10">
+                <div class="field hidden sm:block col-2">
+                    <!-- Definitivamente hacerlo componente -->
+                    <br>
+                    <span class="text-900 font-medium text-xs mb-4">{{ products.length }} Productos Relacionados</span>
+                    <br>
+                    <br>
+                    <BasicFilterSelection @v-model="getfilterprices" :key="componentKey" :allcategories="priceorder" filtername="Filtro por Precios" pickone
+                    :selected="filterpriceselection"/>
+                </div>
+                <div class="field block sm:hidden col-12">
+                    <Sidebar v-model:visible="visible">
+                        <span class="text-900 font-medium text-4xl mb-4">{{ pagetitle }}</span>
+                        <br>
+                        <span class="text-900 font-medium text-xs mb-4">{{ products.length }} Productos Relacionados</span>
+                        <br>
+                        <br>
+                        <BasicFilterSelection @v-model="getfilterprices" :key="componentKey" :allcategories="priceorder" filtername="Filtro por Precios" pickone
+                        :selected="filterpriceselection"/>
+                    </Sidebar>
+                    <Button icon="pi pi-filter" class="filtter-menu-button border-transparent border-1 shadow-3" @click="visible = true" />
+                </div>
+                <div class="field col-12 sm:col-10">
                     <div class="card">
                         <br>
                         <!-- Agregar paginator obligatorio -->
@@ -44,6 +69,7 @@ const allproducts = ref<any[]>([]);
 const filterpriceselection = ref<number[]>([]);
 const priceorder = ref<any[]>([{"id": 1, "name": "Menor a Mayor"},{"id": 2, "name": "Mayor a Menor"}]);
 const componentKey = ref<number>(0);
+const visible = ref<boolean>(false)
 
 onMounted(async () => {
     await refresh();
@@ -66,11 +92,12 @@ const refresh = async () => {
 
 watch(filterpriceselection, (newvalue)=>{
     if(newvalue[0] == 1){
-        products.value = products.value.sort((a,b) => a.ecomerce_offer_price - b.ecomerce_offer_price)
+        products.value = products.value.sort((a,b) => a.price_tax - b.price_tax)
     }else if(newvalue[0] == 2){
-        products.value = products.value.sort((a,b) => b.ecomerce_offer_price - a.ecomerce_offer_price)
+        products.value = products.value.sort((a,b) => b.price_tax - a.price_tax)
     }
     productsKey.value = productsKey.value+1;
+    componentKey.value = componentKey.value+1;
 })
 
 watch(router.currentRoute, async ()=>{
@@ -83,5 +110,15 @@ const getfilterprices = (model) =>{
 </script>
     
 <style>
-    
+    .full-width-sm-index {
+  background-image: url('/public/demo/images/pages/fondo dental-01.jpg');
+  background-size: cover;
+  background-position: center;
+  padding: 50px 20px; /* Reducir padding en pantallas pequeñas */
+  color: rgb(255, 255, 255);
+  display: flex;
+  flex-direction: column; /* Cambiar a columna para centrar verticalmente en pantallas pequeñas */
+  align-items: center; /* Center horizontally */
+  text-align: center; /* Center text within each column */
+    }
 </style>
